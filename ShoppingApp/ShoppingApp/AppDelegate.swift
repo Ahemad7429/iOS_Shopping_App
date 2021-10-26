ec00472416e6029feab75971cf9e52d5e6bb8334
Krupal Patel <krupalpatel111098@gmail.com>
2021-10-26 21:30:55 +0530

    Update AppDelegate.swift

--- ShoppingApp/ShoppingApp/AppDelegate.swift
+++ ShoppingApp/ShoppingApp/AppDelegate.swift
@@ -17,13 +17,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        initializePayPal()
-        FirebaseApp.configure()
-        
-        IQKeyboardManager.shared().isEnabled = true
+        uiHalperAndConfigs()
         return true
     }
 
+    func uiHalperAndConfigs() {
+                initializePayPal()
+        FirebaseApp.configure()
+        
+        IQKeyboardManager.shared().isEnabled = true
+    }
+        
     // MARK: UISceneSession Lifecycle
 
     @available(iOS 13.0, *)
